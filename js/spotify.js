a7205a1ea095283748b4ee799681ceaa5e475fda
Alexandra <akscreven@gmail.com>
2015-11-29 14:09:05 -0500

    selector for search button

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,28 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/technologies/web-api/search/
 
 
-function searchByArtist(keyword) {
-  var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
-}
+// function searchByArtist(keyword) {
+//     var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+// }
 
 
-function searchByTrack(keyword) {
-  var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
-}
+// function searchByTrack(keyword) {
+//   var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
+// }
+$(document).ready(function(){
+  $(".submit").on("click", function(keyword){
+    var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    }).done(function(){
+      console.log("Ajax request success!")
+    }).fail(function(){
+      console.log("Ajax request fails!")
+    }).always(function(){
+      console.log("This always happens regardless of successful ajax request or not.")
+    })
+  })
+})
