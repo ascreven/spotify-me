4e5f63e3671e4a93e185706b96e257c2774e4b92
Alexandra <akscreven@gmail.com>
2015-11-29 18:26:21 -0500

    click handler working

--- js/spotify.js
+++ js/spotify.js
@@ -10,19 +10,34 @@
 // function searchByTrack(keyword) {
 //   var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
 // }
+
+
 $(document).ready(function(){
-  $(".submit").on("click", function(keyword){
-    var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
-    $.ajax({
-      url: url,
-      type: "get",
-      dataType: "json"
-    }).done(function(){
-      console.log("Ajax request success!")
-    }).fail(function(){
-      console.log("Ajax request fails!")
-    }).always(function(){
-      console.log("This always happens regardless of successful ajax request or not.")
-    })
-  })
-})
+  userInput = document.querySelector("#search-keyword")
+
+});
+
+var searchButton = document.querySelector(".submit");
+
+  var searchClickEvent = function(){
+    console.log('I was clicked');
+  };
+
+searchButton.addEventListener('click', searchClickEvent);
+// )
+//   $(".submit").on("click", function(keyword){
+//     var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+//     $.ajax({
+//       url: url,
+//       type: "get",
+//       dataType: "json"
+//
+//     }).done(function(response){
+//       console.log(response)
+//     }).fail(function(){
+//       console.log("Ajax request fails")
+//     }).always(function(){
+//       console.log("This always happens regardless of successful ajax request or not.")
+//     })
+//   })
+// })
