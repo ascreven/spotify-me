883717d9bb5eb93107175c553fe45694d8ffd22e
Alexandra <akscreven@gmail.com>
2015-11-29 19:50:29 -0500

    test

--- js/spotify.js
+++ js/spotify.js
@@ -6,24 +6,20 @@
 //     var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
 // }
 
-
 // function searchByTrack(keyword) {
 //   var url = 'http://ws.spotify.com/search/1/track.json?q='+keyword;
 // }
 $(document).ready(function(){
-  $(".submit").on("click", function(keyword){
-    console.log(keyword)
-    var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
-    $.ajax({
-      url: url,
-      type: "get",
-      dataType: "json"
-    }).done(function(){
-      console.log("Ajax request success!")
-    }).fail(function(){
-      console.log("Ajax request fails!")
-    }).always(function(){
-      console.log("This always happens regardless of successful ajax request or not.")
-    })
-  })
+  searchFor = $( "#search-type" ).val();
+  $(".submit").on("click", artistOrTrack(searchFor));
 })
+
+function artistOrTrack(selection) {
+  if(selection === 'artist') {
+    // searchByArtist();
+    console.log('artist')
+  } else {
+    // searchByTrack();
+    console.log('track')
+  }
+}
