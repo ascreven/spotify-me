6bda52797ef9784727236d67dc3812a1c1847d7d
Alexandra <akscreven@gmail.com>
2015-11-29 21:49:30 -0500

    lists first artist result

--- js/spotify.js
+++ js/spotify.js
@@ -12,20 +12,26 @@ $(document).ready(function(){
   $(".submit").on("click", function(event) {
     event.preventDefault();
     searchInput = $('#search-keyword').val();
-    searchByArtist(searchInput)
-  })
+    searchByArtist(searchInput);
+  });
 
-function searchByArtist(keyword) {
-  $('ul').append('<li>' + keyword + '</li>');
-  var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
-  $.ajax({
-    type: 'GET',
-    dataType: 'json',
-    url: url
-  }).done(function(response) {
-    console.log(response);
-  }).fail(function(response){
-    console.log("Ajax get request failed.");
-  })
+  function searchByArtist(keyword) {
+    var url = 'http://ws.spotify.com/search/1/artist.json?q='+keyword;
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: url
+    }).done(function(response) {
+      results = response;
+      listResults(results);
+
+    }).fail(function(response){
+      console.log("Ajax get request failed.");
+    });
+  }
+});
+
+function listResults(list) {
+  console.log(list.artists[0].name)
+    $('ul').append('<li>' + list.artists[0].name + '</li>');
 }
-})
